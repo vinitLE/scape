89b4671dc866354277f2314605232bf31ed64e29
vinitLE <71550354+vinitLE@users.noreply.github.com>
2022-06-05 13:49:26 +0530

    adding backend

--- scapemain/indexsign.js
+++ scapemain/indexsign.js
@@ -21,59 +21,63 @@ db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"));
 
 
-app.post("/done1",(req,res)=>{
+app.post("/done1",async(req,res)=>{
+
+    try{
+
     var username = req.body.username;
     var password = req.body.password;
     
+  
+        const userdetail =await db.collection("register").findOne({username:username});
+        if(userdetail.pass==password){
+            res.redirect("submitdata.html")
+        }else{
+            res.send("password in not matching")
+        }
+    }catch{
+        res.status(400).send("invalid email")
+    }
+})
+
+app.post("/done2",(req,res)=>{
+
+    try{
+
+        
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var pass = req.body.pass;
+    var confpass = req.body.confpass;
 
     
 
     var data = {
+        "name": name,
+        "email" : email,
         "username": username,
-        "password" : password,
-        
+        "pass" :pass,
+        "confpass": confpass
     }
+    if(pass==confpass){
 
-    db.collection('ScapeUsers').insertOne(data,(err,collection)=>{
+    db.collection('register').insertOne(data,(err,collection)=>{
         if(err){
             throw err;
         }
         console.log("Record Inserted Successfully");
     });
-
+    
     return res.redirect('index.html')
 
+    }else{
+        res.send("password not matching");
+    }
+    }catch{
+           res.status(400).send(error);
+    }
 })
-
-// app.post("/done2",(req,res)=>{
-//     var name = req.body.name;
-//     var usn = req.body.usn;
-//     var college = req.body.college;
-//     var dob = req.body.dob;
-//     var city = req.body.city;
-//     var teachdis = req.body.teachdis;
-
-    
-
-//     var data = {
-//         "name": name,
-//         "usn" : usn,
-//         "college": college,
-//         "dob" : dob,
-//         "city": city,
-//         "teachdis": teachdis
-//     }
-
-//     db.collection('teach').insertOne(data,(err,collection)=>{
-//         if(err){
-//             throw err;
-//         }
-//         console.log("Record Inserted Successfully");
-//     });
-
-//     return res.redirect('submit.html')
-
-// })
 
 // app.post("/done3",(req,res)=>{
 //     var name = req.body.name;
